9cabe154c06bde6e58b53ff97638b4133ab14f10
iwatakira <iwata.kira@compass-japan.com>
2024-04-19 14:47:44 +0900

    ログイン時に入力したデータを保存してマイページに移行させるプログラム作成

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -3,15 +3,27 @@ package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("Kintai/*")
 @Controller
 public class LoginController {
-	@GetMapping("Login")
-	public String open(Model model) {
-		String str = "ログイン画面";
-		model.addAttribute("value", str);
-		return "Login";
-	}
+  @GetMapping("Login")
+  public String getlogin(Model model) {
+    String str = "ログイン画面";
+    model.addAttribute("value", str);
+    
+    return "Login";
+  }
+  
+  @PostMapping("Kintai/Login")
+  public String postLogin(@RequestParam("UserId")String str,@RequestParam("")String str2,Model model) {
+	  
+	  model.addAttribute("ID",str);
+	  model.addAttribute("PASS",str2);
+	  
+	  return "MyPage";
+  }
 }
--- src/main/java/com/example/demo/Controller/MyPageController.java
+++ src/main/java/com/example/demo/Controller/MyPageController.java
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("Kintai/*")
+@RequestMapping(path="Kintai/*", method=RequestMethod.POST)
 @Controller
 public class MyPageController {
   @GetMapping("MyPage")
   public String open(Model model) {
     String str = "マイページ";
     model.addAttribute("value", str);
-    return "Mypage";
+    return "MyPage";
   }
 }
