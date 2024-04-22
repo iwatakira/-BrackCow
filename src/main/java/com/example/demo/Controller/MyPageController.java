f14736545765ee90526f97d58e749466066038fb
TomoharuAKAMATSU <165618697+TomoharuAKAMATSU@users.noreply.github.com>
2024-04-22 10:29:17 +0900

    IDを用いたSQLからの名前取得の仮コード

--- src/main/java/com/example/demo/Controller/MyPageController.java
+++ src/main/java/com/example/demo/Controller/MyPageController.java
@@ -1,5 +1,8 @@
 package com.example.demo.Controller;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +15,40 @@ public class MyPageController {
 	@GetMapping("MyPage")
 	public String open(@RequestParam("UserId")String str,@RequestParam("Pass")String str2,Model model) {
 		String str3 = "ユーザーマイページ";
+		
+		Connection conn = null;
+		Statement stmt = null;
+		
+		String name = "ユーザーネーム_初期値";
+		//String aaa = null;;
+		//String bbb = null;;
+		//String ccc = null;;
+		
+		// 仮のSQLからのデータ取得
+		/*try {
+			ResultSet rs = stmt.executeQuery("SELECT " + str + "from userDataBase;");
+			System.out.println("Fetching result set...");
+		  
+			while (rs.next()) {
+				name = rs.getString(1);
+				//String aaa = rs.getString(1);
+				//String bbb = rs.getString(1);
+				//String ccc = rs.getString(1);
+			  
+			}
+			
+		    System.out.println("\n Customer number: " + name);
+		    //System.out.println(" Region: " + aaa);
+		    //System.out.println(" Name: " + bbb);
+		    //System.out.println(" Address: " + ccc);
+		    
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}*/
+		
 		model.addAttribute("value3", str3);
-		model.addAttribute("ID",str);
+		model.addAttribute("ID",name);
 		
 		return "MyPage";
 	}
