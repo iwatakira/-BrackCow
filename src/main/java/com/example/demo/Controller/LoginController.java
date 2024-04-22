b4a2f59b03ddf7d389e34cd0bc137d68f8c34714
iwatakira <iwata.kira@compass-japan.com>
2024-04-22 09:43:23 +0900

    入力したユーザーIDとパスワードをMyPageに送るようにした

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -3,7 +3,9 @@ package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("Kintai/*")
 @Controller
@@ -13,5 +15,14 @@ public class LoginController {
 		String str = "ログイン画面";
 		model.addAttribute("value", str);
 		return "Login";
+	}
+
+	@PostMapping("Kintai/MyPage")
+	public String postLogin(@RequestParam("UserId")String str,@RequestParam("Pass")String str2,Model model) {
+		
+		model.addAttribute("ID",str);
+		model.addAttribute("PASS",str2);
+		
+		return "MyPage";
 	}
 }
