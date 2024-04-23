cb91d8a6c9d83fb424a18fb5c2195041d8bbd202
TomoharuAKAMATSU <165618697+TomoharuAKAMATSU@users.noreply.github.com>
2024-04-23 16:22:06 +0900

    Loginページから受け取ったIDデータを用いて、MySQLから名前の取得、表示と勤怠管理ページへのIDデータの受け渡しコードの作成

--- src/main/java/com/example/demo/Controller/MyPageController.java
+++ src/main/java/com/example/demo/Controller/MyPageController.java
@@ -1,30 +1,42 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.database.EmployeeData;
+import com.example.demo.database.EmployeeRepository;
 
 @RequestMapping("Kintai/*")
 @Controller
 public class MyPageController {
+	@Autowired
+	EmployeeRepository empRepo;
+	
 	@GetMapping("MyPage")
-	public String open(@RequestParam("UserId")String str,@RequestParam("Pass")String str2,Model model) {
-		String str3 = "ユーザーマイページ";
+	public String openMyPage(@RequestParam("UserId")int str,@RequestParam("Pass")String str2,Model model) {
+		String title = "ユーザーマイページ";
+		String Name = "ユーザーネーム_初期値";
 		
-		String name = "ユーザーネーム_初期値";
+		EmployeeData em = empRepo.getFromID(str);
+		Name = em.getName();
 		
-		//以下にIDを用いて、MySQLから名前の取得をする。
-		
-		
-		//ここまで
-		
-		model.addAttribute("value3", str3);
-		
-		model.addAttribute("Name",name);
+		model.addAttribute("Title", title);
+		model.addAttribute("Name",Name);
 		model.addAttribute("ID",str);
 		
 		return "MyPage";
+	}
+	
+	@PostMapping("check")
+	public String postInOut(@RequestParam("UserId")int str,@RequestParam("Pass")String str2, Model model) {
+		
+		model.addAttribute("UserId", str);
+				
+		return "InOut";
 	}
 }
