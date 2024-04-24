6c2bdb820c7259524eb4348215a77849210f72fe
ooguri <ooguri.yuto@compass-japan.com>
2024-04-24 10:12:14 +0900

    管理者画面でID取得できるようにしました

--- src/main/java/com/example/demo/Controller/AdminController.java
+++ src/main/java/com/example/demo/Controller/AdminController.java
@@ -1,17 +1,43 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.database.EmployeeData;
+import com.example.demo.database.EmployeeRepository;
 
 @RequestMapping("Kintai/*")
 @Controller
 public class AdminController {
-  @GetMapping("Admin")
-  public String open(Model model) {
-    String str = "管理者画面";
-    model.addAttribute("value", str);
-    return "Admin";
-  }
+	@Autowired
+	EmployeeRepository empRepo;
+	
+	@GetMapping("Admin")
+	public String open(@RequestParam("UserId")int str,Model model) {
+		String title = "管理者ページ";
+		String Name = "管理者ネーム_初期値";
+		
+		EmployeeData em = empRepo.getFromID(str);
+		Name = em.getName();
+		
+		model.addAttribute("Title", title);
+		model.addAttribute("Name",Name);
+		model.addAttribute("ID",str);
+		
+		return "Admin";
+	}
+	
+	@PostMapping("Log")
+	public String postInOut(@RequestParam("UserId")int str, Model model) {
+		
+		model.addAttribute("UserId", str);
+				
+		return "Log";
+	}
+
 }
