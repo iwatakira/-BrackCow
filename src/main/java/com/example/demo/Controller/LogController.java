bf38082bfd2c9d08076562e7dcd455422cfbc7cd
yk22375006 <yk22375006@ga.ttc.ac.jp>
2024-04-25 09:09:40 +0900

    最終形態

--- src/main/java/com/example/demo/Controller/LogController.java
+++ src/main/java/com/example/demo/Controller/LogController.java
@@ -1,18 +1,39 @@
 package com.example.demo.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.database.EmployeeData;
+import com.example.demo.database.EmployeeRepository;
+
 @RequestMapping("Kintai/*")
 @Controller
 public class LogController {
-  @GetMapping("Log")
-  public String open(Model model) {
-    String str = "従業員管理画面";
-    model.addAttribute("value", str);
-    return "Log";
-  }
+
+	@Autowired
+	private EmployeeRepository emplRepo;
+	
+	@GetMapping("Log")
+	public String open(Model model) {
+				
+		String str = "従業員管理画面";
+		model.addAttribute("value", str);
+		
+		List<EmployeeData> dataList  = emplRepo.getAllData();
+
+		int num = emplRepo.count();	
+								
+		model.addAttribute("users", dataList);
+		
+		return "Log";
+	}
+	
+	
 }
+
 
